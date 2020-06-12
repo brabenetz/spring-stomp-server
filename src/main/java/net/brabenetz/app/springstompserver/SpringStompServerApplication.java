20946be384beef8bfb47cf88c20fe65b3b3b90ba
Harald Brabenetz <brabenetz@gmx.net>
2020-06-12 14:39:03 +0200

    initial checkin only created by https://start.spring.io/ with WebSocket Dependency.

---
+++ src/main/java/net/brabenetz/app/springstompserver/SpringStompServerApplication.java
@@ -0,0 +1,13 @@
+package net.brabenetz.app.springstompserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStompServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStompServerApplication.class, args);
+	}
+
+}
