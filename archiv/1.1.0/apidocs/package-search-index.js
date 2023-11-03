0f4250183cd36a9517828a68ae6d21f1970a00a9
Harald Brabenetz <brabenetz@gmx.net>
2023-11-03 11:27:25 +0100

    update documentation for 1.1.0

---
+++ archiv/1.1.0/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"net.brabenetz.app.springstompserver"},{"l":"net.brabenetz.app.springstompserver.config"}];updateSearchResults();
