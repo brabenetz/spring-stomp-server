6d457642f45b796c8ddddd3dfdde6893f14ebfa0
Harald Brabenetz <brabenetz@gmx.net>
2023-11-03 11:30:49 +0100

    update symlinks for 1.0.1

--- archiv/updateLatest-SymLink.bat
+++ archiv/updateLatest-SymLink.bat
@@ -1,12 +1,12 @@
 rmdir latest
 rm latest
-mklink /D latest 1.0.1
+mklink /D latest 1.1.0
 rmdir current
 rm current
-mklink /D current 1.0.1
+mklink /D current 1.1.0
 cd ..
 rmdir currentrelease
 rm currentrelease
-mklink /D currentrelease archiv\1.0.1
+mklink /D currentrelease archiv\1.1.0
 cd archiv
 pause
